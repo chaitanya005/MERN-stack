3fd278d36f87f559037a958846c4ff5412ef2bdf
github_chaitanyamuvvala <muvvalachaitanya05@gmail.com>
2020-05-04 21:14:21 +0530

    saved user in DB

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,34 @@
+const User = require("../models/user");
+const { check, validationResult } = require("express-validator");
+
+
+//singup
+exports.signup = (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      error: errors.array()[0].msg
+    });
+  }
+
+  const user = new User(req.body);
+  //saving user in DB
+  user.save((err, user) => {
+    if (err) {
+      return res.status(400).json({
+        err: "NOT able to save user in DB"
+      });
+    }
+    res.json({
+      name: user.name,
+      email: user.email,
+      id: user._id
+    });
+  });
+};
+
+//sign out
 exports.signout = (req, res) => {
   res.json({
     message: "User signout"
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,20 @@
 var express = require("express");
 var router = express.Router();
-const { signout } = require("../controllers/auth");
+const { check, validationResult } = require("express-validator");
 
+//importing routes from controllers 
+const { signout, signup } = require("../controllers/auth");
+
+//checkers 
+router.post(
+  "/signup",
+  [
+    check("name", "name should be at least 3 char").isLength({ min: 3 }),
+    check("email", "email is required").isEmail(),
+    check("password", "password should be at least 3 char").isLength({ min: 3 })
+  ],
+  signup
+);
 router.get("/signout", signout);
 
 module.exports = router;
