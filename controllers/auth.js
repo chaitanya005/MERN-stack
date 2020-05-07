7c6df2deb8cc319a931f2a7e22fe3374038fc9e2
Chaitanya Muvvala <muvvalachaitanya05@gmail.com>
2020-05-07 21:31:12 +0530

    adding custom middlewares

--- controllers/auth.js
+++ controllers/auth.js
@@ -81,3 +81,24 @@ exports.isSignedIn = express_jwt({
     secret : process.env.SECRET,
     userProperty: "auth"    //auth grabs the user _id 
 });
+
+
+//custom middlewares
+exports.isAuthenticated = (req, res, next) => {
+  let checker = req.profile && req.auth && req.profile._id === req.auth._id;
+  if (!checker) {
+    return res.status(403).json({
+      error: "ACCESS DENIED"
+    });
+  }
+  next();
+};
+
+exports.isAdmin = (req, res, next) => {
+  if (req.profile.role === 0) {
+    return res.status(403).json({
+      error: "You are not ADMIN, Access denied"
+    });
+  }
+  next();
+};
