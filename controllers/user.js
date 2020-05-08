4736f9a45ce5d0f0c3566dc9c58f710c23515346
Chaitanya Muvvala <muvvalachaitanya05@gmail.com>
2020-05-08 21:36:24 +0530

    displaying order list of the particular user

--- controllers/user.js
+++ controllers/user.js
@@ -43,3 +43,31 @@ exports.updateUser = (req, res) => {
     }
   );
 };
+
+
+exports.userPurchaseList = (req, res) => {
+  Order.find({ user: req.profile._id })
+    .populate("user", "_id name")
+    .exec((err, order) => {
+      if (err) {
+        return res.status(400).json({
+          error: "No Order in this account"
+        });
+      }
+      return res.json(order);
+    });
+};
+
+exports.pushOrderInPurchaseList = (req, res, next) => {
+  let purchases = [];
+  req.body.order.products.forEach(product => {
+    purchases.push({
+      _id: product._id,
+      name: product.name,
+      description: product.description,
+      category: product.category,
+      quantity: product.quantity,
+      amount: req.body.order.amount,
+      transaction_id: req.body.order.transaction_id
+    });
+  });
