8d8636e7cf7bd087c2a106284df4caad11dc5c07
Chaitanya Muvvala <muvvalachaitanya05@gmail.com>
2020-05-08 21:30:18 +0530

    Display all the users from DB

--- controllers/user.js
+++ controllers/user.js
@@ -12,6 +12,7 @@ exports.getUserById = (req, res, next, id) => {
   });
 };
 
+
 exports.getUser = (req, res) => {
   //we are declaring undefined to these fields
   //cause this is a getUser controller and after 
@@ -21,4 +22,24 @@ exports.getUser = (req, res) => {
   req.profile.createdAt = undefined
   req.profile.updatedAt = undefined
   return res.json(req.profile);
+};
+
+
+//to get all the users from DB
+exports.updateUser = (req, res) => {
+  User.findByIdAndUpdate(
+    { _id: req.profile._id },
+    { $set: req.body },
+    { new: true, useFindAndModify: false },
+    (err, user) => {
+      if (err) {
+        return res.status(400).json({
+          error: "You are not authorized to update this user"
+        });
+      }
+      user.salt = undefined;
+      user.encry_password = undefined;
+      res.json(user);
+    }
+  );
 };
