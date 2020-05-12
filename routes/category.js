82a6f780e498b4d98283505d41f6b0af3d4339d2
Chaitanya Muvvala <muvvalachaitanya05@gmail.com>
2020-05-12 21:33:05 +0530

    complete category route

--- routes/category.js
+++ routes/category.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const { getCategoryById, createCategory } = require("../controllers/category");
+const {
+  getCategoryById,
+  createCategory,
+  getCategory,
+  getAllCategory,
+  updateCategory,
+  deleteCategory
+} = require("../controllers/category");
 const { isSignedIn, isAdmin, isAuthenticated } = require("../controllers/auth");
 const { getUserById } = require("../controllers/user");
 
@@ -17,6 +24,27 @@ router.post(
   isAuthenticated,
   isAdmin,
   createCategory
+);
+
+router.get("/category/:categoryId", getCategory);
+router.get("/categories", getAllCategory);
+
+//update
+router.put(
+  "/category/:categoryId/:userId",
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+  updateCategory
+);
+
+//delete
+router.delete(
+  "/category/:categoryId/:userId",
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+  deleteCategory
 );
 
 module.exports = router;
