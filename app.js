8a090c1207313768bf3dad1c7f9c1a0454627efc
Chaitanya Muvvala <muvvalachaitanya05@gmail.com>
2020-05-12 21:38:35 +0530

    Update of app.js file

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
+require("dotenv").config();
+
 const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+//routes
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+const categoryRoutes = require("./routes/category");
+
 
 mongoose
   .connect("mongodb://localhost:27017/myapp", {
@@ -11,6 +22,17 @@ mongoose
   .then(() => {
     console.log("DB CONNECTED");
   });
+
+//Middlewares
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+
+//My Routes
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+app.use("/api", categoryRoutes);
+
 
 //using environmnet variable
 const port = process.env.PORT || 7000;
